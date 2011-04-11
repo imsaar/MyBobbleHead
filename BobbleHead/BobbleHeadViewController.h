1893f2c5277f5d27f787637113d40c43dea11637
Ali Rizvi <imsaar@gmail.com>
2011-04-10 22:27:49 -0700

    Initial raw version with two sliders

--- BobbleHead/BobbleHeadViewController.h
+++ BobbleHead/BobbleHeadViewController.h
@@ -10,6 +10,14 @@
 
 @interface BobbleHeadViewController : UIViewController {
     
+  UIImageView *headImage;
 }
+
+@property (nonatomic, retain) IBOutlet UIImageView *headImage;
+
+- (IBAction)horizontalMovementSlider:(id)sender;
+- (void)moveImage:(UIImageView *)image duration:(NSTimeInterval)duration
+            curve:(int)curve x:(CGFloat)x y:(CGFloat)y;
+- (IBAction)verticalMovementSlider:(id)sender;
 
 @end
--- BobbleHead/BobbleHeadViewController.m
+++ BobbleHead/BobbleHeadViewController.m
@@ -9,9 +9,11 @@
 #import "BobbleHeadViewController.h"
 
 @implementation BobbleHeadViewController
+@synthesize headImage;
 
 - (void)dealloc
 {
+    [headImage release];
     [super dealloc];
 }
 
@@ -35,6 +37,7 @@
 
 - (void)viewDidUnload
 {
+    [self setHeadImage:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -46,4 +49,42 @@
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
+- (void)moveImage:(UIImageView *)image duration:(NSTimeInterval)duration
+            curve:(int)curve x:(CGFloat)x y:(CGFloat)y
+{
+  // Setup the animation
+  [UIView beginAnimations:nil context:NULL];
+  [UIView setAnimationDuration:duration];
+  [UIView setAnimationCurve:curve];
+  [UIView setAnimationBeginsFromCurrentState:YES];
+  
+  // The transform matrix
+  CGAffineTransform transform = CGAffineTransformMakeTranslation(x, y);
+  image.transform = transform;
+  
+  // Commit the changes
+  [UIView commitAnimations];
+  
+}
+
+- (IBAction)verticalMovementSlider:(id)sender {
+  UISlider *verticalSlider = (UISlider *)sender;
+  float vSliderValue = verticalSlider.value;
+  CGRect bounds = [headImage bounds];
+  CGFloat x = bounds.origin.x;
+  CGFloat y = bounds.origin.y;
+  
+  [self moveImage:headImage duration:0.2 curve:UIViewAnimationCurveLinear x:x y:(y + 20.0 * vSliderValue)];
+}
+  
+
+- (IBAction)horizontalMovementSlider:(id)sender {
+  UISlider *horizontalSlider = (UISlider *)sender;
+  float hSliderValue = horizontalSlider.value;
+  CGRect bounds = [headImage bounds];
+  CGFloat x = bounds.origin.x;
+  CGFloat y = bounds.origin.y;
+  
+  [self moveImage:headImage duration:0.2 curve:UIViewAnimationCurveLinear x:(x + 20.0 * hSliderValue) y:y];
+}
 @end
